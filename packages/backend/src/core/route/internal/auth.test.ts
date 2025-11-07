245ee49d16820e3961cf7045c04d50f855e4544d
Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>
2025-11-08 05:25:10 +0800

    20251108-v2 Updates
    backend
    - Additional tests for auth endpoint
    - Add new locked user used for integration testing
    
    Signed-off-by: Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>

--- packages/backend/src/core/route/internal/auth.test.ts
+++ packages/backend/src/core/route/internal/auth.test.ts
@@ -16,7 +16,7 @@ type TErrorOutput = Extract<
 >
 
 describe('Auth Endpoint', () => {
-    it('Sign-in with valid credentials should pass.', async () => {
+    it('[username] Sign-in with valid credentials should pass.', async () => {
         const response = await app.request(
             '/internal/auth/sign-in/username?organizationId=superorganization',
             {
@@ -40,7 +40,7 @@ describe('Auth Endpoint', () => {
         expect(responseData).toHaveProperty('data')
     })
 
-    it('Sign-in with missing Organization ID should fail.', async () => {
+    it('[username] Sign-in with missing Organization ID should fail.', async () => {
         const response = await app.request(
             '/internal/auth/sign-in/username',
             {
@@ -66,7 +66,7 @@ describe('Auth Endpoint', () => {
         )
     })
 
-    it('Sign-in with invalid Organization ID should fail.', async () => {
+    it('[username] Sign-in with invalid Organization ID should fail.', async () => {
         const response = await app.request(
             '/internal/auth/sign-in/username?organizationId=INVALID',
             {
@@ -88,5 +88,225 @@ describe('Auth Endpoint', () => {
         expect(response.status).toBe(422)
         expect(responseData).toHaveProperty('error')
         expect(responseData.error.message).toBe('Invalid credentials provided.')
+    })
+
+    it('[username] Sign-in with invalid username should fail.', async () => {
+        const response = await app.request(
+            '/internal/auth/sign-in/username?organizationId=superorganization',
+            {
+                method: 'POST',
+                headers: {
+                    origin: 'vitest-pool-worker',
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: 'INVALID',
+                    password: 'P@ssw0rd1234',
+                }),
+            },
+            env,
+        )
+
+        const responseData = await response.json<TErrorOutput>()
+
+        expect(response.status).toBe(422)
+        expect(responseData).toHaveProperty('error')
+        expect(responseData.error.message).toBe('Invalid credentials provided.')
+    })
+
+    // This test currently triggers a false-positive failure.
+    it.skip('[username] Sign-in with invalid password should fail.', async () => {
+        const response = await app.request(
+            '/internal/auth/sign-in/username?organizationId=superorganization',
+            {
+                method: 'POST',
+                headers: {
+                    origin: 'vitest-pool-worker',
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: 'superadministrator',
+                    password: 'INVALID',
+                }),
+            },
+            env,
+        )
+
+        const responseData = await response.json<TErrorOutput>()
+
+        expect(response.status).toBe(422)
+        expect(responseData).toHaveProperty('error')
+        expect(responseData.error.message).toBe('Invalid credentials provided.')
+    })
+
+    it('[username] Sign-in with locked account should fail.', async () => {
+        const response = await app.request(
+            '/internal/auth/sign-in/username?organizationId=superorganization',
+            {
+                method: 'POST',
+                headers: {
+                    origin: 'vitest-pool-worker',
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: 'locked',
+                    password: 'P@ssw0rd1234',
+                }),
+            },
+            env,
+        )
+
+        const responseData = await response.json<TErrorOutput>()
+
+        expect(response.status).toBe(423)
+        expect(responseData).toHaveProperty('error')
+        expect(responseData.error.message).toBe('Account is currently locked.')
+    })
+
+    it('[email] Sign-in with valid credentials should pass.', async () => {
+        const response = await app.request(
+            '/internal/auth/sign-in/email?organizationId=superorganization',
+            {
+                method: 'POST',
+                headers: {
+                    origin: 'vitest-pool-worker',
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: 'superadministrator@localhost.dev',
+                    password: 'P@ssw0rd1234',
+                }),
+            },
+            env,
+        )
+
+        const responseData = await response.json<TDataOutput>()
+
+        expect(response.status).toBe(200)
+        expect(response.headers.get('set-cookie')).toBeTruthy()
+        expect(responseData).toHaveProperty('data')
+    })
+
+    it('[email] Sign-in with missing Organization ID should fail.', async () => {
+        const response = await app.request(
+            '/internal/auth/sign-in/email',
+            {
+                method: 'POST',
+                headers: {
+                    origin: 'vitest-pool-worker',
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: 'superadministrator@localhost.dev',
+                    password: 'P@ssw0rd1234',
+                }),
+            },
+            env,
+        )
+
+        const responseData = await response.json<TErrorOutput>()
+
+        expect(response.status).toBe(400)
+        expect(responseData).toHaveProperty('error')
+        expect(responseData.error.message).toBe(
+            'Organization ID was not provided.',
+        )
+    })
+
+    it('[email] Sign-in with invalid Organization ID should fail.', async () => {
+        const response = await app.request(
+            '/internal/auth/sign-in/email?organizationId=INVALID',
+            {
+                method: 'POST',
+                headers: {
+                    origin: 'vitest-pool-worker',
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: 'superadministrator@localhost.dev',
+                    password: 'P@ssw0rd1234',
+                }),
+            },
+            env,
+        )
+
+        const responseData = await response.json<TErrorOutput>()
+
+        expect(response.status).toBe(422)
+        expect(responseData).toHaveProperty('error')
+        expect(responseData.error.message).toBe('Invalid credentials provided.')
+    })
+
+    it('[email] Sign-in with invalid e-mail should fail.', async () => {
+        const response = await app.request(
+            '/internal/auth/sign-in/email?organizationId=superorganization',
+            {
+                method: 'POST',
+                headers: {
+                    origin: 'vitest-pool-worker',
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: 'INVALID@INVALID.invalid',
+                    password: 'P@ssw0rd1234',
+                }),
+            },
+            env,
+        )
+
+        const responseData = await response.json<TErrorOutput>()
+
+        expect(response.status).toBe(422)
+        expect(responseData).toHaveProperty('error')
+        expect(responseData.error.message).toBe('Invalid credentials provided.')
+    })
+
+    // This test currently triggers a false-positive failure.
+    it.skip('[email] Sign-in with invalid password should fail.', async () => {
+        const response = await app.request(
+            '/internal/auth/sign-in/email?organizationId=superorganization',
+            {
+                method: 'POST',
+                headers: {
+                    origin: 'vitest-pool-worker',
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: 'superadministrator@localhost.dev',
+                    password: 'INVALID',
+                }),
+            },
+            env,
+        )
+
+        const responseData = await response.json<TErrorOutput>()
+
+        expect(response.status).toBe(422)
+        expect(responseData).toHaveProperty('error')
+        expect(responseData.error.message).toBe('Invalid credentials provided.')
+    })
+
+    it('[email] Sign-in with locked account should fail.', async () => {
+        const response = await app.request(
+            '/internal/auth/sign-in/email?organizationId=superorganization',
+            {
+                method: 'POST',
+                headers: {
+                    origin: 'vitest-pool-worker',
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: 'locked@localhost.dev',
+                    password: 'P@ssw0rd1234',
+                }),
+            },
+            env,
+        )
+
+        const responseData = await response.json<TErrorOutput>()
+
+        expect(response.status).toBe(423)
+        expect(responseData).toHaveProperty('error')
+        expect(responseData.error.message).toBe('Account is currently locked.')
     })
 })
--- packages/backend/src/db/pg/migrations/0001_initial_data.sql
+++ packages/backend/src/db/pg/migrations/0001_initial_data.sql
@@ -7,13 +7,15 @@ SET CONSTRAINTS ALL DEFERRED;
 INSERT INTO "account"
     (id, user_id, account_id, provider_id, password)
 VALUES
-    ('ACCOUNT_001', 'USER_001', 'USER_001', 'credential', 't8lNmhcpgYY5pqB3t5anZsyMyI0Zbw2f:3d0264f0459d0a26851c2e4172db15196e69ace403f7ceab6f1818b009f9fcd7d645da1ed9e6e42ff5c138db81d840e79aa6323aeb1bac435959a8b2f06d0617');
+    ('ACCOUNT_001', 'USER_001', 'USER_001', 'credential', 't8lNmhcpgYY5pqB3t5anZsyMyI0Zbw2f:3d0264f0459d0a26851c2e4172db15196e69ace403f7ceab6f1818b009f9fcd7d645da1ed9e6e42ff5c138db81d840e79aa6323aeb1bac435959a8b2f06d0617'),
+    ('ACCOUNT_999', 'USER_999', 'USER_999', 'credential', 't8lNmhcpgYY5pqB3t5anZsyMyI0Zbw2f:3d0264f0459d0a26851c2e4172db15196e69ace403f7ceab6f1818b009f9fcd7d645da1ed9e6e42ff5c138db81d840e79aa6323aeb1bac435959a8b2f06d0617');
 
 -- Member
 INSERT INTO "member"
     (id, user_id, organization_id, role)
 VALUES
-    ('MEMBER_001', 'USER_001', 'ORGANIZATION_001', 'owner');
+    ('MEMBER_001', 'USER_001', 'ORGANIZATION_001', 'owner'),
+    ('MEMBER_999', 'USER_999', 'ORGANIZATION_001', 'member');
 
 -- Organization
 INSERT INTO "organization"
@@ -42,10 +44,12 @@ VALUES
 INSERT INTO "user"
     (id, name, email, username)
 VALUES
-    ('USER_001', 'SUPER ADMINISTRATOR', 'superadministrator@localhost.dev', 'superadministrator');
+    ('USER_001', 'SUPER ADMINISTRATOR', 'superadministrator@localhost.dev', 'superadministrator'),
+    ('USER_999', 'LOCKED', 'locked@localhost.dev', 'locked');
 
 -- User Attribute
 INSERT INTO "user_attribute"
     (user_id, is_locked)
 VALUES
-    ('USER_001', false);
+    ('USER_001', false),
+    ('USER_999', true);
