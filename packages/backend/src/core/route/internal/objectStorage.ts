1a55c4191e686bc47320743b32a7d9b2a5238023
Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>
2025-11-21 14:18:43 +0800

    20251121-v2 Updates
    backend
    - Minor revamp of the upload workflow
    
    Signed-off-by: Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>

--- packages/backend/src/core/route/internal/objectStorage.ts
+++ packages/backend/src/core/route/internal/objectStorage.ts
@@ -1,11 +1,15 @@
 import {
     objectStorageCreateDownloadLinkInputSchema,
     objectStorageCreateUploadLinkInputSchema,
+    objectStorageUploadAttachmentAddInputSchema,
 } from '@hyperion/validator/internal/objectStorage'
+import { hexToBytes } from '@noble/hashes/utils.js'
 import { eq, inArray } from 'drizzle-orm'
 import { Hono } from 'hono'
+import { encodeBase64 } from 'hono/utils/encode'
 import { validator } from 'hono/validator'
 
+import { AppError } from '../../../errors.js'
 import { honoValidatorCb, nanoidCustom } from '../../../utilities.js'
 import { isAuthenticated } from '../../middleware/isAuthenticated.js'
 
@@ -108,6 +112,218 @@ objectStorageRoute.post(
         }
 
         return ctx.json({ data: signedUrls }, 200)
+    },
+)
+
+objectStorageRoute.get('/upload/id/create', isAuthenticated(), async (ctx) => {
+    const { upload } = ctx.get('dbSchema')
+
+    try {
+        const uploadId = nanoidCustom(16)
+
+        await ctx
+            .get('dbClient')
+            .insert(upload)
+            .values({
+                id: uploadId,
+                userId: ctx.get('user')!.id,
+            })
+
+        return ctx.json(
+            {
+                data: { uploadId },
+            },
+            200,
+        )
+    } catch (err) {
+        throw new AppError(
+            {
+                status: 500,
+                code: 'UPLOAD_ID_CREATION_FAILED',
+                message: 'Upload ID creation failed.',
+            },
+            err instanceof Error ? err : undefined,
+        )
+    }
+})
+
+objectStorageRoute.post(
+    '/upload/attachment/add',
+    isAuthenticated(),
+    validator('json', async (value, ctx) =>
+        honoValidatorCb(
+            value,
+            ctx,
+            objectStorageUploadAttachmentAddInputSchema,
+        ),
+    ),
+    async (ctx) => {
+        const { attachments, uploadId } = ctx.req.valid('json')
+
+        const { objectStorage, objectStorageAcl, upload, uploadAttachment } =
+            ctx.get('dbSchema')
+
+        const objectStorageData: Pick<
+            typeof objectStorage.$inferInsert,
+            'id' | 'size' | 'mimeType' | 'hashSha256' | 'isPublic'
+        >[] = []
+
+        const objectStorageAclData: Pick<
+            typeof objectStorageAcl.$inferInsert,
+            'userId' | 'objectStorageId'
+        >[] = []
+
+        const uploadAttachmentData: Pick<
+            typeof uploadAttachment.$inferInsert,
+            'uploadId' | 'objectStorageId'
+        >[] = []
+
+        // Check if provided uploadId is valid
+        const uploadData = await ctx
+            .get('dbClient')
+            .select({ id: upload.id })
+            .from(upload)
+            .where(eq(upload.id, uploadId))
+
+        if (!uploadData[0]) {
+            return ctx.json(
+                {
+                    error: {
+                        code: 'BAD_REQUEST',
+                        message: 'Upload ID not found.',
+                    },
+                },
+                400,
+            )
+        }
+
+        const signedUrls: {
+            objectStorageId: string
+            hash: string
+            encodedHash: string | null
+            signedUrl: string | null
+            status: 200 | 409
+        }[] = []
+
+        // Existing SHA-256 hash check
+        const hashesToCheck = attachments.map(({ hashSha256 }) => hashSha256)
+
+        const existingObjects = await ctx
+            .get('dbClient')
+            .select({
+                id: objectStorage.id,
+                hashSha256: objectStorage.hashSha256,
+            })
+            .from(objectStorage)
+            .where(inArray(objectStorage.hashSha256, hashesToCheck))
+
+        const existingHashes = existingObjects.map(
+            ({ hashSha256 }) => hashSha256,
+        )
+
+        // Generate pre-signed upload URLs
+        for (const attachment of attachments) {
+            if (existingHashes.includes(attachment.hashSha256)) {
+                const objectStorageId = existingObjects.filter(
+                    ({ hashSha256 }) => hashSha256 === attachment.hashSha256,
+                )[0].id
+
+                uploadAttachmentData.push({ uploadId, objectStorageId })
+
+                objectStorageAclData.push({
+                    userId: ctx.get('user')!.id,
+                    objectStorageId,
+                })
+
+                signedUrls.push({
+                    objectStorageId,
+                    hash: attachment.hashSha256,
+                    encodedHash: null,
+                    signedUrl: null,
+                    status: 409,
+                })
+            } else {
+                // Encode SHA-256 hash to Base64
+                const hashBase64 = encodeBase64(
+                    hexToBytes(attachment.hashSha256).buffer,
+                )
+
+                objectStorageData.push({
+                    ...attachment,
+                    size: Number(attachment.size),
+                })
+
+                uploadAttachmentData.push({
+                    uploadId,
+                    objectStorageId: attachment.id,
+                })
+
+                objectStorageAclData.push({
+                    userId: ctx.get('user')!.id,
+                    objectStorageId: attachment.id,
+                })
+
+                signedUrls.push({
+                    objectStorageId: attachment.id,
+                    hash: attachment.hashSha256,
+                    encodedHash: hashBase64,
+                    signedUrl: (
+                        await ctx
+                            .get('aws4FetchClient')
+                            .sign(
+                                `https://${ctx.env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com/${ctx.env.CF_R2_BUCKET}/${attachment.id}?X-Amz-Expires=${300}`,
+                                {
+                                    method: 'PUT',
+                                    headers: {
+                                        // https://developers.cloudflare.com/r2/api/s3/api/#checksum-types
+                                        'x-amz-checksum-sha256': hashBase64,
+                                    },
+                                    aws: {
+                                        service: 's3',
+                                        signQuery: true,
+                                    },
+                                },
+                            )
+                    ).url,
+                    status: 200,
+                })
+            }
+        }
+
+        try {
+            await ctx.get('dbClient').transaction(async (tx) => {
+                if (objectStorageData.length > 0) {
+                    // Must throw an error if there is a duplicate Object ID
+                    await tx.insert(objectStorage).values(objectStorageData)
+                }
+
+                await tx
+                    .insert(objectStorageAcl)
+                    .values(objectStorageAclData)
+                    .onConflictDoNothing()
+
+                await tx
+                    .insert(uploadAttachment)
+                    .values(uploadAttachmentData)
+                    .onConflictDoNothing()
+            })
+
+            return ctx.json(
+                {
+                    data: { uploadId, signedUrls },
+                },
+                200,
+            )
+        } catch (err) {
+            throw new AppError(
+                {
+                    status: 500,
+                    code: 'UPLOAD_ATTACHMENT_ADDITION_FAILED',
+                    message: 'Upload attachment addition failed.',
+                },
+                err instanceof Error ? err : undefined,
+            )
+        }
     },
 )
 
--- packages/validator/src/internal/objectStorage.ts
+++ packages/validator/src/internal/objectStorage.ts
@@ -136,3 +136,67 @@ export const objectStorageCreateUploadLinkOutputSchema = baseOutputSchema(
         ),
     }),
 )
+
+export const objectStorageUploadAttachmentAddInputSchema = z.object({
+    uploadId: textField({ fieldName: 'Upload ID', min: 16 }).regex(
+        /^[a-zA-Z0-9]+$/,
+        { error: 'Upload ID must be alphanumeric characters only.' },
+    ),
+    attachments: z
+        .array(
+            z.object({
+                id: textField({ fieldName: 'Attachment ID', min: 32 }).regex(
+                    /^[a-zA-Z0-9]+$/,
+                    {
+                        error: 'Attachment ID must be alphanumeric characters only.',
+                    },
+                ),
+                size: numericField({ fieldName: 'Size', max: 10485760 }),
+                mimeType: textField({ fieldName: 'MIME Type', min: 8 })
+                    .lowercase()
+                    .optional(),
+                hashSha256: textField({
+                    fieldName: 'SHA-256 hash',
+                })
+                    .lowercase()
+                    .check((ctx) => {
+                        if (
+                            !/^[0-9a-fA-F]+$/.test(ctx.value) ||
+                            !(ctx.value.length === 64)
+                        ) {
+                            ctx.issues.push({
+                                code: 'custom',
+                                message: `Invalid SHA-256 hash provided.`,
+                                input: ctx.value,
+                            })
+                        }
+                    }),
+                isPublic: booleanField('isPublic Flag')
+                    .optional()
+                    .default(false),
+            }),
+        )
+        .min(1, { error: 'At least one attachment must be provided.' })
+        .check((ctx) => {
+            if (ctx.value.length > 1) {
+                const allHashes = ctx.value.map(({ hashSha256 }) => hashSha256)
+                const duplicateHashes = Array.from(
+                    new Set(
+                        // If the current value being filtered is found
+                        // in a different index, it is a duplicate :D
+                        allHashes.filter(
+                            (hash, index) => allHashes.indexOf(hash) !== index,
+                        ),
+                    ),
+                )
+
+                if (duplicateHashes.length > 0) {
+                    ctx.issues.push({
+                        code: 'custom',
+                        message: `Duplicate SHA-256 hashes detected. [${duplicateHashes.toString()}]`,
+                        input: duplicateHashes,
+                    })
+                }
+            }
+        }),
+})
