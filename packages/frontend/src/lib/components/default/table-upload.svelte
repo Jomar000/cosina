668d87bebee8ac51c1548743ab27eadfba26376f
Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>
2025-12-09 12:02:06 +0800

    20251209-v1 Updates
    ALL
    - Bump package dependency versions
    
    frontend
    - Minor modifications to table-upload component
    
    Signed-off-by: Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>

--- packages/frontend/src/lib/components/default/table-upload.svelte
+++ packages/frontend/src/lib/components/default/table-upload.svelte
@@ -271,11 +271,14 @@
 
             const responseData = await response.json()
 
-            if ('data' in responseData) {
-                uploadId = responseData.data.uploadId
+            if (!responseData.success) {
+                console.error(`[table-upload] ${responseData.error.message}`)
+                return
             }
+
+            uploadId = responseData.data.uploadId
         } else {
-            //
+            // TODO: Populate fileList with existing data
         }
     })
 </script>
