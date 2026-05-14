756e4645f552a1ef48621deea6607be68398fea1
Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>
2026-05-15 01:30:01 +0800

    20260515-v1 Updates
    ALL
    - Bump package dependency versions
    
    packages/database
    - Add key_value table
    
    Signed-off-by: Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>

--- packages/database/src/postgres/schema.ts
+++ packages/database/src/postgres/schema.ts
@@ -110,6 +110,31 @@ export const auditTrail = pgTable(
     ],
 )
 
+export const keyValue = pgTable('key_value', {
+    id: bigint('id', { mode: 'number' })
+        .generatedByDefaultAsIdentity()
+        .primaryKey(),
+    publicId: uuid('public_id')
+        .unique()
+        .notNull()
+        .default(sql`gen_random_uuid()`)
+        .$defaultFn(() => uuidv7()),
+    key: text('key').unique().notNull(),
+    value: text('value'),
+    createdAt: timestamp('created_at', {
+        withTimezone: true,
+        mode: 'date',
+    })
+        .notNull()
+        .defaultNow(),
+    updatedAt: timestamp('updated_at', {
+        withTimezone: true,
+        mode: 'date',
+    })
+        .notNull()
+        .defaultNow(),
+})
+
 export const objectStorage = pgTable('object_storage', {
     id: text('id').primaryKey(),
     size: bigint('size', { mode: 'number' }).notNull(),
