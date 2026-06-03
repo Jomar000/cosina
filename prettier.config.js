ddfe5260a36291817e03b337fb6e2e099579155a
Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>
2026-06-03 12:01:54 +0800

    20260603-v3 Updates
    root
    - Bump package dependency versions
    - Add new prettier plugin canonical classes
    
    Signed-off-by: Lindjunne Gerard Montenegro II <lgbmontenegroii@4thdevision.tech>

--- prettier.config.js
+++ prettier.config.js
@@ -7,6 +7,7 @@ const prettierConfig = {
     plugins: [
         'prettier-plugin-svelte',
         'prettier-plugin-tailwindcss',
+        'prettier-plugin-tailwindcss-canonical-classes',
         'prettier-plugin-multiline-arrays',
     ],
     semi: false,
